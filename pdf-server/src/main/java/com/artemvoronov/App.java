7ba2096ba43f3546f5968c2c31e5421421a67b57
teman777 <artemkavoronov@gmail.com>
2020-02-27 17:24:20 +0400

    Added config files + component shit

--- pdf-server/src/main/java/com/artemvoronov/App.java
+++ pdf-server/src/main/java/com/artemvoronov/App.java
@@ -2,11 +2,18 @@ package com.artemvoronov;
 import com.artemvoronov.entity.*;
 import com.artemvoronov.service.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class App
 {
-    public static void main( String[] args )
+  @Autowired
+  private BookService bookService;
+
+    public  void notMain()
     {
-      BookService bookService = new BookService();
       Book book = new Book("sdf","dsf","/url/url/");
       book.addBookMark(new BookMark());
       bookService.saveBook(book);
---
+++ pdf-server/src/main/java/com/artemvoronov/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.artemvoronov.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(value={"com.bytestree"})
+public class AppConfig {
+
+}
