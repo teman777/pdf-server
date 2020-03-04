39a80eb7a1c3a4f8626fda87b985e4dd229a2f45
teman777 <artemkavoronov@gmail.com>
2020-03-04 17:46:29 +0400

    Add file upload

--- pdf-server/src/main/java/com/artemvoronov/config/AppConfig.java
+++ pdf-server/src/main/java/com/artemvoronov/config/AppConfig.java
@@ -7,12 +7,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.fasterxml.jackson.databind.Module;
 import org.springframework.context.annotation.Bean;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 @ComponentScan(value={"com.artemvoronov"})
 @EnableWebMvc
 public class AppConfig {
 
-  
+  @Bean(name="multipartResolver")
+  public CommonsMultipartResolver multipartResolver() {
+    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+    multipartResolver.setMaxUploadSize(10000000);
+    return multipartResolver;
+  }
+
+
 
 }
--- pdf-server/src/main/java/com/artemvoronov/controller/BookController.java
+++ pdf-server/src/main/java/com/artemvoronov/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.artemvoronov.controller;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import com.artemvoronov.entity.*;
 import com.artemvoronov.service.BookService;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.nio.charset.StandardCharsets;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -67,6 +70,11 @@ public class BookController {
       return ResponseEntity.notFound().build();
     }
     bookService.deleteBook(bookService.findBook(id));
+    return ResponseEntity.ok().build();
+  }
+
+  @RequestMapping(value="/book/{id}/uploadFile", method=RequestMethod.POST)
+  public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable int id){
     return ResponseEntity.ok().build();
   }
 
